c33ac0ec675c9796ef6a667e5f7804b0d6ae505c
memoryleak47 <memoryleak47@protonmail.ch>
2026-08-25 15:49:25 +0200

    remove stuff from prior version

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,6 @@ pub use ast::*;
 mod parse;
 pub use parse::*;
 
-mod visit;
-pub use visit::*;
-
-mod nameres;
-pub use nameres::*;
-
-mod nil_init;
-pub use nil_init::*;
-
-mod analysis;
-pub use analysis::*;
-
-mod optimize;
-pub use optimize::*;
-
-mod layout;
-pub use layout::*;
-
-mod comp;
-pub use comp::*;
-
 extern crate symbol_table;
 pub type Symbol = symbol_table::GlobalSymbol;
 
@@ -39,15 +18,6 @@ fn main() {
     let path = root.join("examples").join(filename + ".gradu");
     let s = std::fs::read_to_string(path).unwrap();
 
-    let mut ast = parse(&s);
-    let mut nameres = nameres(&ast);
-    nil_init(&mut ast, &nameres);
-    let actxt = loop {
-        let actxt = analyze(&ast, &nameres);
-        if !optimize(&mut ast, &mut nameres, &actxt) {
-            break actxt
-        }
-    };
-    let lctxt = layout(&ast, &nameres, &actxt);
-    comp::comp(&ast, &nameres, &lctxt);
+    let ast = parse(&s);
+    dbg!(ast);
 }
