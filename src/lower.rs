3126818a3396bfa703befb9d4c6d20f598ca01af
memoryleak47 <memoryleak47@protonmail.ch>
2026-08-27 20:33:33 +0200

    rewrite of the lowering pipeline via FnCtxt

--- src/lower.rs
+++ src/lower.rs
@@ -3,69 +3,66 @@ use crate::*;
 use std::collections::{BTreeSet, BTreeMap};
 use crate::ir::{Terminator, BlkDef, FnDef, ValueId, Ty, BlkId, FnId, AppliedBlk};
 
-fn fresh_blk(fdef: &mut FnDef, varmap: &BTreeMap<Symbol, ValueId>) -> BlkId {
-    let new = fresh();
-    fdef.blocks.insert(new, BlkDef {
-        args: (0..varmap.len()).map(|_| fresh()).collect(),
-        stmts: Vec::new(),
-        terminator: Terminator::Exit,
-        types: HashMap::new(),
-    });
-    new
+// The context that governs lowering a particular function.
+struct FnCtxt {
+    fn_id: FnId,
+    vars: Vec<Symbol>, // list of variables.
+    blockvars: HashMap<BlkId, Vec<ValueId>>, // expresses which variable corresponds to which ValueId at the last position of any block.
+    current: BlkId, // do we want this?
+    ir: IR,
 }
 
-fn mk_applied_blk(target: BlkId, varmap: &BTreeMap<Symbol, ValueId>) -> AppliedBlk {
-    (target, varmap.values().copied().collect())
-}
-
-fn lower_binop(kind: BinOpKind, l: ValueId, r: ValueId, out: &mut BlkDef) -> ir::Expr {
+fn lower_binop(kind: BinOpKind, l: ValueId, r: ValueId, ctxt: &mut FnCtxt) -> ir::Expr {
     use BinOpKind::*;
 
     match kind {
         Plus|Minus|Mod => {
-            let l = mk_expr(ir::Expr::ValueToT(l, Ty::Int), out);
-            let r = mk_expr(ir::Expr::ValueToT(r, Ty::Int), out);
-            let o = mk_expr(ir::Expr::BinOp(kind, l, r), out);
+            let l = mk_expr(ir::Expr::ValueToT(l, Ty::Int), ctxt);
+            let r = mk_expr(ir::Expr::ValueToT(r, Ty::Int), ctxt);
+            let o = mk_expr(ir::Expr::BinOp(kind, l, r), ctxt);
             ir::Expr::TToValue(o, Ty::Int)
         },
         Gt|Lt => {
-            let l = mk_expr(ir::Expr::ValueToT(l, Ty::Int), out);
-            let r = mk_expr(ir::Expr::ValueToT(r, Ty::Int), out);
-            let o = mk_expr(ir::Expr::BinOp(kind, l, r), out);
+            let l = mk_expr(ir::Expr::ValueToT(l, Ty::Int), ctxt);
+            let r = mk_expr(ir::Expr::ValueToT(r, Ty::Int), ctxt);
+            let o = mk_expr(ir::Expr::BinOp(kind, l, r), ctxt);
             ir::Expr::TToValue(o, Ty::Bool)
         },
         Equ|Ne => {
-            let o = mk_expr(ir::Expr::BinOp(kind, l, r), out);
+            let o = mk_expr(ir::Expr::BinOp(kind, l, r), ctxt);
             ir::Expr::TToValue(o, Ty::Bool)
         },
         x => todo!("{x:?}"),
     }
 }
 
-fn lower_expr(e: &ast::Expr, out: &mut BlkDef, varmap: &BTreeMap<Symbol, ValueId>) -> ValueId {
+fn lower_expr(e: &ast::Expr, ctxt: &mut FnCtxt) -> ValueId {
     let e = match e {
         ast::Expr::StringLit(x) => {
-            let a = mk_expr(ir::Expr::StringLit(x.clone()), out);
+            let a = mk_expr(ir::Expr::StringLit(x.clone()), ctxt);
             ir::Expr::TToValue(a, Ty::String)
         },
         ast::Expr::IntLit(x) => {
-            let a = mk_expr(ir::Expr::IntLit(*x), out);
+            let a = mk_expr(ir::Expr::IntLit(*x), ctxt);
             ir::Expr::TToValue(a, Ty::Int)
         },
         ast::Expr::BoolLit(x) => {
-            let a = mk_expr(ir::Expr::BoolLit(*x), out);
+            let a = mk_expr(ir::Expr::BoolLit(*x), ctxt);
             ir::Expr::TToValue(a, Ty::Bool)
         },
         ast::Expr::BinOp(kind, l, r) => {
-            let l = lower_expr(l, out, varmap);
-            let r = lower_expr(r, out, varmap);
-            lower_binop(*kind, l, r, out)
+            let l = lower_expr(l, ctxt);
+            let r = lower_expr(r, ctxt);
+            lower_binop(*kind, l, r, ctxt)
         },
-        ast::Expr::Var(v) => return varmap[v],
+        ast::Expr::Var(v) => {
+            let i = var_idx(*v, ctxt);
+            return ctxt.blockvars[&ctxt.current][i]
+        },
         x => todo!("{x:?}"),
     };
 
-    mk_expr(e, out)
+    mk_expr(e, ctxt)
 }
 
 fn ty_of(e: &ir::Expr) -> Ty {
@@ -82,172 +79,255 @@ fn ty_of(e: &ir::Expr) -> Ty {
     }
 }
 
-fn mk_expr(e: ir::Expr, out: &mut BlkDef) -> ValueId {
+fn mk_expr(e: ir::Expr, ctxt: &mut FnCtxt) -> ValueId {
     let fresh = fresh();
-    out.types.insert(fresh, ty_of(&e));
-    out.stmts.push(ir::Stmt::Compute(fresh, e));
+    let bdef = ctxt.get_bdef();
+    bdef.types.insert(fresh, ty_of(&e));
+    bdef.stmts.push(ir::Stmt::Compute(fresh, e));
     fresh
 }
 
-fn lower_blk(stmts: &[ast::Stmt], out: &mut FnDef, post: BlkId, varmap: &mut BTreeMap<Symbol, ValueId>) -> BlkId {
-    let new = fresh_blk(out, varmap);
-    let mut new_mut = new;
+fn lower_blk(stmts: &[ast::Stmt], terminator: Terminator, ctxt: &mut FnCtxt) -> BlkId {
+    let old = ctxt.current;
 
+    let new = ctxt.fresh_blk();
+
+    ctxt.focus_blk(new);
     for st in stmts {
-        lower_stmt(st, out, &mut new_mut, varmap);
+        lower_stmt(st, ctxt);
     }
-    out.blocks.get_mut(&new_mut).unwrap().terminator = Terminator::Goto(mk_applied_blk(post, varmap));
+    ctxt.set_terminator(terminator);
+
+    ctxt.focus_blk(old);
 
     new
 }
 
-fn lower_stmt(stmt: &ast::Stmt, out: &mut FnDef, bid: &mut BlkId, varmap: &mut BTreeMap<Symbol, ValueId>) {
+fn lower_stmt(stmt: &ast::Stmt, ctxt: &mut FnCtxt) {
     match stmt {
         ast::Stmt::Print(x) => {
-            let v = lower_expr(x, out.blocks.get_mut(bid).unwrap(), varmap);
-            out.blocks.get_mut(bid).unwrap().stmts.push(ir::Stmt::Print(v));
+            let v = lower_expr(x, ctxt);
+            ctxt.push_stmt(ir::Stmt::Print(v));
         },
         ast::Stmt::If(cond, then_, else_) => {
-            let cond = lower_expr(cond, out.blocks.get_mut(bid).unwrap(), varmap);
-            let cond = mk_expr(ir::Expr::ValueToT(cond, Ty::Bool), out.blocks.get_mut(bid).unwrap());
+            let cond = lower_expr(cond,  ctxt);
+            let cond = mk_expr(ir::Expr::ValueToT(cond, Ty::Bool), ctxt);
 
-            let post = fresh_blk(out, varmap);
+            let post = ctxt.fresh_blk();
 
-            let then_ = lower_blk(then_, out, post, varmap);
-            let else_ = lower_blk(else_, out, post, varmap);
+            let then_ = lower_blk(then_, Terminator::Goto(ctxt.mk_applied_blk(post)), ctxt);
+            let else_ = lower_blk(else_, Terminator::Goto(ctxt.mk_applied_blk(post)), ctxt);
 
-            out.blocks.get_mut(bid).unwrap().terminator = Terminator::IfGoto(cond, mk_applied_blk(then_, varmap), mk_applied_blk(else_, varmap));
+            ctxt.set_terminator(Terminator::IfGoto(cond, ctxt.mk_applied_blk(then_), ctxt.mk_applied_blk(else_)));
 
-            *bid = post;
+            ctxt.focus_blk(post);
         },
 
         ast::Stmt::While(cond, body) => {
-            let head = fresh_blk(out, varmap);
-            let post = fresh_blk(out, varmap);
-            out.blocks.get_mut(bid).unwrap().terminator = Terminator::Goto(mk_applied_blk(head, varmap));
+            let head = ctxt.fresh_blk();
+            let post = ctxt.fresh_blk();
 
-            let cond = lower_expr(cond, out.blocks.get_mut(&head).unwrap(), varmap);
-            let cond = mk_expr(ir::Expr::ValueToT(cond, Ty::Bool), out.blocks.get_mut(&head).unwrap());
+            ctxt.set_terminator(Terminator::Goto(ctxt.mk_applied_blk(head)));
 
-            let body = lower_blk(body, out, head, varmap);
+            let body = lower_blk(body, Terminator::Goto(ctxt.mk_applied_blk(head)), ctxt);
 
-            out.blocks.get_mut(&head).unwrap().terminator = Terminator::IfGoto(cond, mk_applied_blk(body, varmap), mk_applied_blk(post, varmap));
+            ctxt.focus_blk(head);
 
-            *bid = post;
+            let cond = lower_expr(cond, ctxt);
+            let cond = mk_expr(ir::Expr::ValueToT(cond, Ty::Bool), ctxt);
+
+            ctxt.set_terminator(Terminator::IfGoto(cond, ctxt.mk_applied_blk(body), ctxt.mk_applied_blk(post)));
+
+            ctxt.focus_blk(post);
         },
         ast::Stmt::Assign(var, val) => {
-            let val = lower_expr(val, out.blocks.get_mut(bid).unwrap(), varmap);
-            varmap.insert(*var, val);
+            let val = lower_expr(val, ctxt);
+
+            let i = var_idx(*var, ctxt);
+            ctxt.blockvars.get_mut(&ctxt.current).unwrap()[i] = val;
         },
         x => todo!("{x:?}"),
     }
 }
 
 pub fn lower(ast: &AST) -> IR {
-    let mut ir = IR {
+    let ir = IR {
         fns: HashMap::new(),
         global_types: HashMap::new(),
         start: 0,
     };
-    let start = lower_fn(&[], ast, &mut ir);
+    let (start, mut ir) = lower_fn(&[], ast, ir);
     ir.start = start;
     ir
 }
 
-fn lower_fn(args: &[Symbol], body: &[ast::Stmt], ir: &mut IR) -> FnId {
-    let mut vars = BTreeSet::new();
-    get_vars(body, &mut vars);
-    vars.extend(args);
+fn var_idx(v: Symbol, ctxt: &FnCtxt) -> usize {
+    ctxt.vars.iter().position(|v2| v == *v2).unwrap()
+}
+
+fn lower_fn(args: &[Symbol], body: &[ast::Stmt], mut ir: IR) -> (FnId, IR) {
+    let vars = get_vars(args, body);
+
+    let fname = fresh();
 
     let mut blocks = HashMap::new();
 
-    let b1 = fresh();
-    let b2 = fresh();
+    let start = fresh();
     let nil1 = fresh();
     let nil2 = fresh();
 
-    let args1: Vec<_> = (0..args.len()).map(|_| fresh()).collect();
-    let args2: Vec<_> = (0..vars.len()).map(|_| fresh()).collect();
-
-    let varmap: BTreeMap<Symbol, ValueId> = vars.iter().map(|x| {
-        let o = if let Some(i) = args.iter().position(|y| y == x) {
-            args2[i]
-        } else {
-            nil2
-        };
-
-        (*x, o)
-    }).collect();
-
     let mut types = HashMap::new();
+
     types.insert(nil1, Ty::Nil);
     types.insert(nil2, Ty::Value);
-    let mut bdef1 = BlkDef {
-        args: args1,
+
+    let args_v: Vec<ValueId> = (0..args.len()).map(|_| fresh()).collect();
+
+    blocks.insert(start, BlkDef {
+        args: args_v.clone(),
         stmts: vec![
             ir::Stmt::Compute(nil1, ir::Expr::NilLit),
             ir::Stmt::Compute(nil2, ir::Expr::TToValue(nil1, Ty::Nil)),
         ],
-        terminator: Terminator::Goto(mk_applied_blk(b2, &varmap)),
-        types,
-    };
-    blocks.insert(b1, bdef1);
-
-    let mut bdef2 = BlkDef {
-        args: args2.clone(),
-        stmts: Vec::new(),
         terminator: Terminator::Exit,
-        types: HashMap::new(),
+        types,
+    });
+
+    ir.fns.insert(fname, FnDef { blocks, start});
+
+    let mut ctxt = FnCtxt {
+        blockvars: HashMap::new(),
+        current: start,
+        fn_id: fname,
+        vars,
+        ir,
     };
-    blocks.insert(b2, bdef2);
 
-    let mut fdef = FnDef {
-        blocks,
-        start: b1,
-    };
+    let b2 = lower_blk(body, Terminator::Exit, &mut ctxt);
+    let app = ctxt.vars.iter().map(|x|
+        if let Some(i) = args.iter().position(|y| y == x) {
+            args_v[i]
+        } else {
+            nil2
+        }
+    ).collect();
+    ctxt.set_terminator(Terminator::Goto((b2, app)));
 
-    let mut varmap: BTreeMap<Symbol, ValueId> = vars.iter().copied().zip(args2.iter().copied()).collect();
-
-    let mut current = b2;
-    for st in body {
-        lower_stmt(st, &mut fdef, &mut current, &mut varmap);
-    }
-    let fname = fresh();
-    ir.fns.insert(fname, fdef);
-    fname
+    (fname, ctxt.ir)
 }
 
-fn get_vars(body: &[ast::Stmt], out: &mut BTreeSet<Symbol>) {
+// FnCtxt //
+
+impl FnCtxt {
+    fn get_blockvars(&self) -> &[ValueId] {
+        &self.blockvars[&self.current]
+    }
+
+    fn get_bdef(&mut self) -> &mut BlkDef {
+        let fdef = self.ir.fns.get_mut(&self.fn_id).unwrap();
+        fdef.blocks.get_mut(&self.current).unwrap()
+    }
+
+    fn set_terminator(&mut self, terminator: Terminator) {
+        self.get_bdef().terminator = terminator;
+    }
+
+    fn push_stmt(&mut self, stmt: ir::Stmt) {
+        self.get_bdef().stmts.push(stmt);
+    }
+
+    fn focus_blk(&mut self, bid: BlkId) {
+        self.current = bid;
+    }
+
+    fn fresh_argless_blk(&mut self) -> BlkId {
+        let old = self.current;
+
+        let new = fresh();
+        self.focus_blk(new);
+
+        let fdef = self.ir.fns.get_mut(&self.fn_id).unwrap();
+        fdef.blocks.insert(new, BlkDef {
+            args: Vec::new(),
+            stmts: Vec::new(),
+            terminator: Terminator::Exit,
+            types: HashMap::new(),
+        });
+
+        self.focus_blk(old);
+
+        new
+    }
+
+    fn fresh_blk(&mut self) -> BlkId {
+        let old = self.current;
+
+        let new = self.fresh_argless_blk();
+        self.focus_blk(new);
+
+        for x in self.vars.clone() {
+            let v = self.fresh_blkarg(Ty::Value);
+            self.blockvars.get_mut(&new).unwrap().push(v);
+        }
+
+        self.focus_blk(old);
+
+        new
+    }
+
+    fn fresh_blkarg(&mut self, ty: Ty) -> ValueId {
+        let out = fresh();
+        let bdef = self.get_bdef();
+        bdef.types.insert(out, ty);
+        bdef.args.push(out);
+        out
+    }
+
+    fn mk_applied_blk(&self, target: BlkId) -> AppliedBlk {
+        (target, self.get_blockvars().iter().copied().collect())
+    }
+}
+
+// get_vars //
+
+fn get_vars(args: &[Symbol], body: &[ast::Stmt]) -> Vec<Symbol> {
+    let mut out = args.iter().copied().collect();
+    get_vars2(body, &mut out);
+    out.into_iter().collect()
+}
+
+fn get_vars2(body: &[ast::Stmt], out: &mut BTreeSet<Symbol>) {
     for a in body {
         use ast::Stmt::*;
         match a {
             Global(s) => { out.insert(*s); },
-            Return(e)|Print(e) => get_vars_expr(e, out),
-            Assign(v, e) => { out.insert(*v); get_vars_expr(e, out); },
-            Push(e1, e2) => { get_vars_expr(e1, out); get_vars_expr(e2, out); },
-            ListStore(e1, e2, e3) | DictStore(e1, e2, e3) => { get_vars_expr(e1, out); get_vars_expr(e2, out); get_vars_expr(e3, out); },
-            If(e, b1, b2) => { get_vars_expr(e, out); get_vars(b1, out); get_vars(b2, out); },
-            While(e, b) => { get_vars_expr(e, out); get_vars(b, out); },
+            Return(e)|Print(e) => get_vars2_expr(e, out),
+            Assign(v, e) => { out.insert(*v); get_vars2_expr(e, out); },
+            Push(e1, e2) => { get_vars2_expr(e1, out); get_vars2_expr(e2, out); },
+            ListStore(e1, e2, e3) | DictStore(e1, e2, e3) => { get_vars2_expr(e1, out); get_vars2_expr(e2, out); get_vars2_expr(e3, out); },
+            If(e, b1, b2) => { get_vars2_expr(e, out); get_vars2(b1, out); get_vars2(b2, out); },
+            While(e, b) => { get_vars2_expr(e, out); get_vars2(b, out); },
         }
     }
 }
 
-fn get_vars_expr(e: &ast::Expr, out: &mut BTreeSet<Symbol>) {
+fn get_vars2_expr(e: &ast::Expr, out: &mut BTreeSet<Symbol>) {
     use ast::Expr::*;
     match e {
         Var(v) => { out.insert(*v); },
         IndexList(e1, e2) | IndexDict(e1, e2) | BinOp(_, e1, e2) => {
-            get_vars_expr(e1, out); get_vars_expr(e2, out);
+            get_vars2_expr(e1, out); get_vars2_expr(e2, out);
         },
         Length(e) => {
-            get_vars_expr(e, out);
+            get_vars2_expr(e, out);
         },
         FnCall(e, args) => {
-            get_vars_expr(e, out);
+            get_vars2_expr(e, out);
             for a in args {
-                get_vars_expr(a, out);
+                get_vars2_expr(a, out);
             }
         },
         Input|NewList|NewDict|Fn(..)|IntLit(_)|StringLit(_)|BoolLit(_)|NilLit => {},
     }
 }
+
